578c33909b68cda6162745dda609a80dab7da991
Antoni Castagne <anto.casatgne@hotmail.fr>
2023-12-20 13:39:50 +0100

    ajout animation page apropos + debut ajout page logement dynamique

--- src/pages/logement.jsx
+++ src/pages/logement.jsx
@@ -1,14 +1,30 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import Data from "../datas/appartements.json"
 import Header from "../components/header/header"
 import Carousel from "../components/carousel/carousel"
 import Footer from "../components/footer/footer"
+import Error from './error'
+import "../styles/main.scss"
 
 function Logement() {
+    let {id} = useParams()
+    let logements = Data
+    let logement = logements.find((item) => {
+        if (item.id == id) {  
+        return true;
+        }
+        return false;
+    })
     return (
         <div>
             <Header />
-            
-            
+            {logements.map((datas) => (
+            <img src={datas.pictures} />
+            ))}
             <Footer />
+           
         </div>
     )
 }
